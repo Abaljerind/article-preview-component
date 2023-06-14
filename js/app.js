8b42e83ad90547b4b39f9c71ee67ff1ddeb7f47b
Abaljerind <abaljerinda@gmail.com>
2023-06-14 14:41:59 +0700

    create 4 variables

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// button share in mobile
+const buttonShare = document.querySelector("#button-share");
+const mobileButton = document.querySelector(".mobile-button");
+
+// share mode in mobile
+const shareMode = document.querySelector("#share-mode");
+const mobileShare = document.querySelector(".mobile-share");
