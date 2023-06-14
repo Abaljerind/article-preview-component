c401f196c2f9efce0833fad1697bbdd184716c6c
Abaljerind <abaljerinda@gmail.com>
2023-06-14 14:50:55 +0700

    add click method + link in index.html

--- js/app.js
+++ js/app.js
@@ -5,3 +5,17 @@ const mobileButton = document.querySelector(".mobile-button");
 // share mode in mobile
 const shareMode = document.querySelector("#share-mode");
 const mobileShare = document.querySelector(".mobile-share");
+
+buttonShare.addEventListener("click", function () {
+  mobileButton.classList.remove("flex");
+  mobileButton.classList.add("hidden");
+  mobileShare.classList.remove("hidden");
+  mobileShare.classList.add("flex");
+});
+
+shareMode.addEventListener("click", function () {
+  mobileShare.classList.remove("flex");
+  mobileShare.classList.add("hidden");
+  mobileButton.classList.remove("hidden");
+  mobileButton.classList.add("flex");
+});
